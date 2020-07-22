11f68caba04b9ad085750b2502ebb91ab76993c8
Roshan <nahsorad@gmail.com>
2020-07-22 12:37:37 +0545

    Add examples

---
+++ examples/example-reference-error.js
@@ -0,0 +1,19 @@
+const { exec } = require("child_process");
+const path = require("path");
+
+const parseError = require("../index.js");
+
+const filePath = path.resolve(
+	__dirname,
+	"executable-js-files",
+	"throws-reference-error.js"
+);
+
+exec(`node ${filePath}`, (error, stdout, stderr) => {
+	if (stderr.trim() !== "") {
+		const _parsed = parseError(filePath, stderr, stdout);
+		console.dir(_parsed);
+	} else {
+		console.log(stdout);
+	}
+});
